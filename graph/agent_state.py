4eb59f6beeb977da810945431f14ecd1098509b5
Кузнецов Максим Дмитриевич <makskuznetsov19@gmail.com>
2026-05-18 15:34:10 +0300

    Minor fixes

--- graph/agent_state.py
+++ graph/agent_state.py
@@ -1,7 +1,7 @@
 from typing import Annotated, TypedDict, Optional
 from langgraph.graph.message import add_messages
 from langchain_core.messages import BaseMessage
-from graph.models import AnalysisResult
+from graph.models import AnalysisResult, ExtendedAnalysis
 
 
 class AgentState(TypedDict):
@@ -12,3 +12,4 @@ class AgentState(TypedDict):
     target_column: Optional[str]
     problem_description: Optional[str]
     data_issues: Optional[list[str]]
+    extended_analysis: Optional[ExtendedAnalysis]
--- graph/models.py
+++ graph/models.py
@@ -10,3 +10,9 @@ class AnalysisResult(BaseModel):
     target_column: str = Field(description="Поле, которое в датафрейме отвечает за target - предсказываемое значение")
     problem_description: str = Field(description="Общее резюме решаемой задачи и имеющихся данных")
     data_issues: list[str] = Field(description="Описание найденных проблем в данных: пропуски, выбросы и т.д.")
+
+class ExtendedAnalysis(BaseModel):
+    id_columns: list[str] = Field(description="Список колонок, которые потенциально являются идентификаторами")
+    date_string: list[str] = Field(description="Список колонок, которые являтся датами, но хранятся в формате str")
+    high_missing_columns: dict = Field(description="Список колонок с высоким порогом")
+    categorica_columns: list[str] = Field(description="Список колонок, которые являются категориальными переменными")
